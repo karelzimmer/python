49936d3c70a2fa57f692cd5d032f9941940999e1
Karel Zimmer <karel.zimmer@gmail.com>
2025-05-04 09:38:16 +0200

    Replace argparse with getopt

--- getopt_example.py
+++ getopt_example.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 ###############################################################################
-# SPDX-FileComment: Vervang argparse door getopt in Python scripts /
-#                   Replace argparse with getopt in Python scripts
+# SPDX-FileComment: Replace argparse with getopt in Python scripts
+#
 # Vervang: https://docs.python.org/3/library/argparse.html
 #    door: https://docs.python.org/3/library/getopt.html
 #
 #          getopt - C-style parser for command line options
 #
-# # SPDX-FileCopyrightText: Karel Zimmer <info@karelzimmer.nl>
+# SPDX-FileCopyrightText: Karel Zimmer <info@karelzimmer.nl>
 # SPDX-License-Identifier: CC0-1.0
 ###############################################################################
 
@@ -25,13 +25,13 @@ import sys
 ###############################################################################
 
 PROGRAM_NAME: str = 'getopt_example.py'
-PROGRAM_DESC: str = 'Vervang argparse door getopt in Python scripts'
+PROGRAM_DESC: str = 'Replace argparse with getopt in Python scripts'
 
 OPTIONS_SHORT: str = 'hmuv'
 OPTIONS_SHORT += 'g'
 OPTIONS_SHORT += 'f:'       # option -f requires argument
 OPTIONS_LONG: list = ['help', 'manual', 'usage', 'version']
-OPTIONS_LONG += ['gui']     # Combineren alg. opties met spec. opties is mog.
+OPTIONS_LONG += ['gui']     # Combine common options with specific options
 OPTIONS_LONG += ['file=']   # option --file requires argument
 
 ###############################################################################
@@ -48,8 +48,8 @@ err: str = ''
 opt: str = ''
 opts: list = []
 
-print(f'# Inhoud OPTIONS_SHORT: {OPTIONS_SHORT}')
-print(f'# Inhoud OPTIONS_LONG : {OPTIONS_LONG}\n')
+print(f'# Content OPTIONS_SHORT: {OPTIONS_SHORT}')
+print(f'# Content OPTIONS_LONG : {OPTIONS_LONG}\n')
 
 try:
     opts, args = getopt.gnu_getopt(sys.argv[1:], OPTIONS_SHORT, OPTIONS_LONG)
@@ -57,20 +57,22 @@ except getopt.error as err:
     print(f'{PROGRAM_NAME}: {err}')
     sys.exit(1)
 
+print(f'# Content opts: {opts}\n')
+
 for opt, arg in opts:
     if opt in ('-h', '--help'):
-        print(f'{PROGRAM_NAME}: optie {opt} opgegeven')
+        print(f'{PROGRAM_NAME}: option {opt} specified')
     elif opt in ('-m', '--manual'):
-        print(f'{PROGRAM_NAME}: optie {opt} opgegeven')
+        print(f'{PROGRAM_NAME}: option {opt} specified')
     elif opt in ('-u', '--usage'):
-        print(f'{PROGRAM_NAME}: optie {opt} opgegeven')
+        print(f'{PROGRAM_NAME}: option {opt} specified')
     elif opt in ('-v', '--version'):
-        print(f'{PROGRAM_NAME}: optie {opt} opgegeven')
+        print(f'{PROGRAM_NAME}: option {opt} specified')
     elif opt in ('-f', '--file'):
-        print(f'{PROGRAM_NAME}: optie {opt} opgegeven')
-        print(f'{PROGRAM_NAME}: > argument {arg} opgegeven')
+        print(f'{PROGRAM_NAME}: option {opt} specified')
+        print(f'{PROGRAM_NAME}: > argument {arg} specified')
     else:
         # Zou niet voor moeten komen als OPTIONS_SHORT/_SHORT 1:1 met if's. 
-        print(f'{PROGRAM_NAME}: {opt}: niet-afgehandelde optie')
+        print(f'{PROGRAM_NAME}: {opt}: internal error')
 
-print(f'\n# Opgegeven argumenten: {args}')
+print(f'\n# specified arguments: {args}')
